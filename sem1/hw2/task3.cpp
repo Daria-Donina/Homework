4b8e653143b627f1e1c3889dbce3b129124b95ac
Daria-Donina <donina.daria@yandex.ru>
2018-10-16 16:32:54 +0300

    the last version was uploaded accidentally

--- sem1/hw2/task3.cpp
+++ sem1/hw2/task3.cpp
@@ -1,59 +1,111 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void swap(int &firstElement, int &secondElement)
+{
+	const int temp = firstElement;
+	firstElement = secondElement;
+	secondElement = temp;
+}
+
+void bubbleSorting(int *array, int length)
+{
+	for (int i = length - 1; i >= 0; --i)
+	{
+		for (int j = i - 1; j >= 0; --j)
+		{
+			if (array[i] < array[j])
+			{
+				swap(array[i], array[j]);
+			}
+		}
+	}
+}
+
+void quickSorting(int *array, int length)
+{
+	int maxElement = array[0], minElement = array[0];
+	for (int i = 1; i < length; ++i)
+	{
+		if (array[i] > maxElement)
+		{
+			maxElement = array[i];
+		}
+		if (array[i] < minElement)
+		{
+			minElement = array[i];
+		}
+	}
+	int *auxiliaryArray = new int[maxElement - minElement + 1]{};
+	for (int i = 0; i < length; ++i)
+	{
+		auxiliaryArray[array[i] - minElement]++;
+		array[i] = 0;
+	}
+	int count = 0;
+	for (int i = 0; i <= maxElement - minElement; ++i)
+	{
+		while (auxiliaryArray[i] != 0)
+		{
+			array[count] = i + minElement;
+			count++;
+			auxiliaryArray[i]--;
+		}
+	}
+	delete[] auxiliaryArray;
+}
+
+bool testBubble()
+{
+	const int length = 10;
+	int array1[length] = { 9, 3, 6, 3, -1, -9, 7, 6, -10, 0 };
+	bubbleSorting(array1, length);
+	int array2[length] = { 7, 8, 43, 6, -80, -90, 4, 0, 2, 1 };
+	bubbleSorting(array2, length);
+	for (int i = 0; i < 9; ++i)
+	{
+		if (array1[i] > array1[i + 1] && array2[i] > array2[i + 1])
+		{
+			return false;
+		}
+		return true;
+	}
+}
+
+bool testQuick()
+{
+	const int length = 10;
+	int array1[length] = { 9, 3, 6, 3, -1, -9, 7, 6, -10, 0 };
+	quickSorting(array1, length);
+	int array2[length] = { 7, 8, 43, 6, -80, -90, 4, 0, 2, 1 };
+	quickSorting(array2, length);
+	for (int i = 0; i < 9; ++i)
+	{
+		if (array1[i] > array1[i + 1] && array2[i] > array2[i + 1])
+		{
+			return false;
+		}
+		return true;
+	}
+}
+
 int main()
 {
-	printf("Enter the number of array elements\n");
-	int number = 0;
-	scanf("%d", &number);
-	if (number > 0)
+	if (testBubble())
 	{
-		printf("Enter %d", number);
-		printf(" elements of the array\n");
-		int *array = new int[number] {};
-		for (int i = 0; i < number; ++i)
-		{
-			scanf("%d", &array[i]);
-		}
-		int maxElement = array[0], minElement = array[0];
-		for (int i = 1; i < number; ++i)
-		{
-			if (array[i] > maxElement)
-			{
-				maxElement = array[i];
-			}
-			if (array[i] < minElement)
-			{
-				minElement = array[i];
-			}
-		}
-		int *auxiliaryArray = new int[maxElement - minElement + 1]{};
-		for (int i = 0; i < number; ++i)
-		{
-			auxiliaryArray[array[i] - minElement]++;
-			array[i] = 0;
-		}
-		int count = 0;
-		for (int i = 0; i <= maxElement - minElement; ++i)
-		{
-			while (auxiliaryArray[i] != 0)
-			{
-				array[count] = i + minElement;
-				count++;
-				auxiliaryArray[i]--;
-			}
-		}
-		printf("The sorted array is");
-		for (int i = 0; i < number; ++i)
-		{
-			printf(" %d", array[i]);
-		}
+		printf("Tests for bubble sorting passed\n");
 	}
 	else
 	{
-		printf("The input data is incorrect");
+		printf("Tests for bubble sorting failed\n");
 	}
-//	delete[] array;
-//	delete[] auxiliaryArray;
+	if (testQuick())
+	{
+		printf("Tests for quick sorting passed\n");
+	}
+	else
+	{
+		printf("Tests for quick sorting failed\n");
+	}
 	return 0;
 }
