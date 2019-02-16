460c45340036781e4a408f134fca70fd67d903a8
Daria-Donina <donina.daria@yandex.ru>
2019-02-16 15:33:16 +0300

    added factorial counting

---
+++ sem2/hw1/1.1/1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Factorial of 1: {Factorial(1)}");
+            Console.WriteLine($"Factorial of 5: {Factorial(5)}");
+        }
+
+        private static int Factorial(int n)
+            => n <= 1 ? 1 : n * Factorial(n - 1);
+    }
+}
