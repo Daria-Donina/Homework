03b806c087297e831302f1926d4f3b0faaa763dc
Daria-Donina <donina.daria@yandex.ru>
2019-02-16 18:12:48 +0300

    removed spaces

--- sem2/hw1/1.2/1.2/Program.cs
+++ sem2/hw1/1.2/1.2/Program.cs
@@ -21,17 +21,14 @@ namespace _1._2
             {
                 return -1;
             }
-
             else if (n == 0)
             {
                 return 0;
             }
-
             else if (n == 1)
             {
                 return 1;
             }
-
             else
             {
                 return Fibonacci(n - 1) + Fibonacci(n - 2);
