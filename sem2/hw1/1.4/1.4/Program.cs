13283cd953762da0290b29a3ab1b1db708f86335
Daria-Donina <donina.daria@yandex.ru>
2019-02-24 14:04:10 +0300

    fixed some mistakes, added project files

--- sem2/hw1/1.4/1.4/Program.cs
+++ sem2/hw1/1.4/1.4/Program.cs
@@ -10,14 +10,21 @@ namespace _1._4
     {
         static void Main(string[] args)
         {
-            Console.Write($"Enter dimesion of array: ");
+            Console.Write($"Enter dimesion of array (odd number): ");
             int n = Int32.Parse(Console.ReadLine());
             Console.WriteLine();
 
-            Random rand = new Random();
+            var rand = new Random();
+
+            if (n % 2 != 1 || n < 0)
+            {
+                Console.WriteLine("Entered number is not correct");
+                return;
+            }
 
             Console.WriteLine($"The array:");
-            int[,] array = new int[n, n];
+
+            var array = new int[n, n];
             for (int i = 0; i < n; ++i)
             {
                 for (int j = 0; j < n; ++j)
@@ -30,12 +37,13 @@ namespace _1._4
 
             Console.WriteLine();
             Console.WriteLine($"Spiral output:");
-            Output(array, n);
+            Output(array);
             Console.WriteLine();
         }
 
-        static void Output(int[,] array, int n)
+        static void Output(int[,] array)
         {
+            int n = array.GetLength(0);
             int i = n / 2;
             int j = n / 2;
             int center = array[i, j];
@@ -45,32 +53,16 @@ namespace _1._4
             {
                 if (elements % 2 == 1)
                 {
-                    for (int k = 1; k <= elements; ++k)
-                    {
-                        Console.Write($"{array[i , j + k]} ");
-                    }
-                    j = j + elements;
+                    StepsRight(array, elements, ref i, ref j);
 
-                    for (int k = 1; k <= elements; ++k)
-                    {
-                        Console.Write($"{array[i + k, j]} ");
-                    }
-                    i = i + elements;
+                    StepsDown(array, elements, ref i, ref j);
                 }
 
                 if (elements % 2 == 0)
                 {
-                    for (int k = 1; k <= elements; ++k)
-                    {
-                        Console.Write($"{array[i, j - k]} ");
-                    }
-                    j = j - elements;
+                    StepsLeft(array, elements, ref i, ref j);
 
-                    for (int k = 1; k <= elements; ++k)
-                    {
-                        Console.Write($"{array[i - k, j]} ");
-                    }
-                    i = i - elements;
+                    StepsUp(array, elements, ref i, ref j);
                 }
             }
 
@@ -78,6 +70,42 @@ namespace _1._4
             {
                 Console.Write($"{array[0, j]} ");
             }
+        }
+
+        private static void StepsRight(int[,] array, int elements, ref int i, ref int j)
+        {
+            for (int k = 1; k <= elements; ++k)
+            {
+                Console.Write($"{array[i, j + k]} ");
+            }
+            j = j + elements;
+        }
+
+        private static void StepsDown(int[,] array, int elements, ref int i, ref int j)
+        {
+            for (int k = 1; k <= elements; ++k)
+            {
+                Console.Write($"{array[i + k, j]} ");
+            }
+            i = i + elements;
+        }
+
+        private static void StepsLeft(int[,] array, int elements, ref int i, ref int j)
+        {
+            for (int k = 1; k <= elements; ++k)
+            {
+                Console.Write($"{array[i, j - k]} ");
+            }
+            j = j - elements;
+        }
+
+        private static void StepsUp(int[,] array, int elements, ref int i, ref int j)
+        {
+            for (int k = 1; k <= elements; ++k)
+            {
+                Console.Write($"{array[i - k, j]} ");
+            }
+            i = i - elements;
         }
     }
 }
