f0861bc237177c620dbc8bc64ba9bb71c4b0644b
Daria-Donina <donina.daria@yandex.ru>
2019-03-21 20:08:51 +0300

    made stack implemented through linked list and interface of stack

---
+++ sem2/hw2/StackCalculator/StackCalculator/IStack.cs
@@ -0,0 +1,10 @@
+﻿namespace StackCalculator
+{
+    public interface IStack
+    {
+        bool Push(char value);
+        int Pop();
+        bool IsEmpty();
+        int Peek();
+    }
+}
---
+++ sem2/hw2/StackCalculator/StackCalculator/StackList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace StackCalculator
+{
+    class StackList
+    {
+        private class StackElement
+        {
+            public int Value { get; set; }
+            public StackElement Next { get; set; }
+
+            public StackElement(int newValue, StackElement newNext)
+            {
+                Value = newValue;
+                Next = newNext;
+            }
+        }
+
+        private StackElement head;
+
+        public bool Push(char value)
+        {
+            if (!char.IsDigit(value))
+            {
+                return false;
+            }
+
+            head = new StackElement(value, null);
+            return true;
+        }
+
+        private bool IsEmpty() => head == null;
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Removing from the empty stack");
+            }
+
+            var temp = Peek();
+            head = head.Next;
+            return temp;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Peek from the empty stack");
+            }
+
+            return head.Value;
+        }
+    }
+}
