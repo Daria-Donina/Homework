3b0d7876dffcd051a3d027f3e5494023baccd88a
Daria-Donina <donina.daria@yandex.ru>
2019-05-22 17:00:00 +0300

    pattern of the future game

---
+++ sem2/hw6/ConsoleGame/ConsoleGame/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    public class Game
+    {
+        public void OnLeft(object sender, EventArgs args)
+        {
+            Console.CursorLeft -= 1;
+        }
+
+        public void OnRight(object sender, EventArgs args)
+        {
+            Console.CursorLeft += 1;
+        }
+
+        public void OnUp(object sender, EventArgs args)
+        {
+            Console.CursorTop -= 1;
+        }
+
+        public void OnDown(object sender, EventArgs args)
+        {
+            Console.CursorTop += 1;
+        }
+    }
+}
---
+++ sem2/hw6/ConsoleGame/ConsoleGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleGame
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new StreamReader("..\\..\\data.txt");
+
+            var eventLoop = new EventLoop();
+            var game = new Game();
+
+            eventLoop.LeftHandler += game.OnLeft;
+            eventLoop.RightHandler += game.OnRight;
+            eventLoop.UpHandler += game.OnUp;
+            eventLoop.DownHandler += game.OnDown;
+
+            eventLoop.Run();
+        }
+    }
+}
