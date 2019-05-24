929c134f5fa7fd4d6b1f4837d7ece6da6c105c25
Daria-Donina <donina.daria@yandex.ru>
2019-05-24 15:17:44 +0300

    task 1

---
+++ sem2/test3/UIProgram/UIProgram/Form.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace UIProgram
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.startButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.startButton.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(280, 105);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(341, 111);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start progressing";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.OnStartButtonClick);
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 406);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(878, 138);
+            this.progressBar.TabIndex = 1;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.closeButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.closeButton.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.Location = new System.Drawing.Point(344, 263);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(209, 52);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close an application";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Visible = false;
+            this.closeButton.Click += new System.EventHandler(this.OnCloseButtonClick);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.TimerTick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(878, 544);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(900, 600);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
