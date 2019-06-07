2a7748902d8fb428ad57e4a424c550d5b689333f
Daria-Donina <donina.daria@yandex.ru>
2019-06-07 18:04:32 +0300

    added clock

---
+++ sem2/hw7/Clock/Clock/ClockForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class ClockForm : Form
+    {
+        public ClockForm()
+        {
+            InitializeComponent();
+            updatingTimer.Start();
+        }
+
+        public void UpdateTimerTick(object sender, EventArgs e) => timeLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+    }
+}
